618409e350c8f3a4ce7f42ed3331fcc21463a051
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2023-06-23 12:25:17 +0300

    Update parserCrypto.js

--- parserCrypto.js
+++ parserCrypto.js
@@ -8,7 +8,7 @@ async function getCrypto (){
     }
   
   try{
-    const browser = await puppeteer.launch({headless:'new', 'ignoreDefaultArgs': [ '--enable-automation'] })
+    const browser = await puppeteer.launch({headless:true, 'ignoreDefaultArgs': [ '--enable-automation'] })
     const newPage = await browser.newPage();
     await newPage.goto('https://tradingeconomics.com/crypto',{timeout: 0})
 
